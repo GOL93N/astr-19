dce10d024cddcd382eeebd89fcfe62e4d06d247c
GOL93N <cgoldsw4l@gmail.com>
2026-04-20 16:58:21 -0700

    Add files via upload

---
+++ CodeJournal1Prompt2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:24:29 2026
+
+@author: robgo
+"""
+
+#coding journal 1 promt 2
+#Write a Python program (.py file) that prints the sum of two 
+#floating point numbers, the difference between two integers, 
+#and the product of a floating point number and an integer. 
+#In each case, have the program print out the data type of 
+#the resulting answer.
+
+
+num1 = 5.32
+num2 = 7.67
+int1 = 9
+
+ques1 = num1 + num2
+ques2 = num1 - num2
+ques3 = num1 * int1
+
+print(ques1)
+print(type(ques1))
+print(ques2)
+print(type(ques2))
+print(ques3)
+print(type(ques3))
