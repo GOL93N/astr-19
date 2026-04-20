fd9b531f16e76ff4919fbb7d9bf443c45082daff
GOL93N <cgoldsw4l@gmail.com>
2026-04-20 16:56:27 -0700

    Add files via upload

---
+++ codejournal1promt1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 16:20:36 2026
+
+@author: robgo
+"""
+
+#coding journal 1 promt 1
+
+name = input("Whats your name?: ")
+movie = input("Whats your favorite movie and why?: ")
+food = input("Whats your favorite food?: ")
+
+print(f"Hello my name is {name}. {movie} and my favorite food is {food}")
